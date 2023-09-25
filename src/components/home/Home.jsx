eba196a708e918df261f23c63b440116878614a2
Yurii <fanfjrfj589@gmail.com>
2023-09-25 16:36:50 +0200

    fix bags

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -57,7 +57,7 @@ function Home({data, setData, thisLogin, setThisLogin, thisPassword, setThisPass
 
   useEffect(() => {
     fetchData();
-  }, [thisLogin, thisPassword, fetchData]);
+  }, [thisLogin, thisPassword]);
 
   const hadleSupport = () => {
     window.open('https://github.com/dizer2');
